f2953d91d5d0c1b7aace9e0f3063db86671d5cc4
Valera Kamelkov <tahaburrent@gmail.com>
2022-11-28 01:34:45 +0200

    !

--- authorization.js
+++ authorization.js
@@ -1,7 +1,7 @@
 const database = [
   { name: "admin", mail: "admin", pass: 123 },
   {
-    name: "Valera_Kamelkov",
+    name: "Valera Kamelkov",
     mail: "bmm2322.vkamelkov@dakkkim.edu.ua",
     pass: "qazwer213",
   },
@@ -12,5 +12,15 @@ const button = document.querySelector("#submit");
 button.addEventListener("click", () => {
   let inputMail = document.querySelector("#mail");
   let inputPass = document.querySelector("#pass");
-  return console.log(inputMail.value);
+  let backdrop = document.querySelector(".backdrop--authorization");
+  if (
+    database.find((el) => el.mail == inputMail.value) &&
+    database.find((el) => el.pass == inputPass.value)
+  ) {
+    backdrop.classList.add("hide");
+  } else if (inputMail.value == false || inputPass.value == false) {
+    alert("Заповніть всі поля!");
+  } else {
+    alert("У вас нема доступу!");
+  }
 });
