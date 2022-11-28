675762c8fea6fe95f2c72a53fd0452554812282e
Valera Kamelkov <tahaburrent@gmail.com>
2022-11-28 23:06:24 +0200

    Update authorization.js

--- authorization.js
+++ authorization.js
@@ -1,22 +1,23 @@
 const database = [
-  { name: "admin", mail: "admin", pass: 123 },
+  { name: "admin", mail: "admin", pass: 213 },
   {
     name: "Valera Kamelkov",
     mail: "bmm2322.vkamelkov@dakkkim.edu.ua",
     pass: "qazwer213",
   },
+  {
+    name: "Pavlo Dyshlliuk",
+    mail: "bmm2322.pdyshliuk@dakkkim.edu.ua",
+    pass: 78963,
+  },
 ];
 
 const button = document.querySelector("#submit");
-
+let inputMail = document.querySelector("#mail");
+let inputPass = document.querySelector("#pass");
+let backdrop = document.querySelector(".backdrop--authorization");
 button.addEventListener("click", () => {
-  let inputMail = document.querySelector("#mail");
-  let inputPass = document.querySelector("#pass");
-  let backdrop = document.querySelector(".backdrop--authorization");
-  if (
-    database.find((el) => el.mail == inputMail.value) &&
-    database.find((el) => el.pass == inputPass.value)
-  ) {
+  if (check()) {
     backdrop.classList.add("hide");
     document.querySelector(".modal-open").classList.remove("modal-open");
   } else if (inputMail.value == false || inputPass.value == false) {
@@ -25,3 +26,10 @@ button.addEventListener("click", () => {
     alert("У вас нема доступу!");
   }
 });
+const check = () => {
+  for (const data of database) {
+    if (data.mail == inputMail.value && data.pass == inputPass.value) {
+      return true;
+    }
+  }
+};
