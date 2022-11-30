f6e1c8473f02d28e555c61c8e58cf4aad43045d3
Valera Kamelkov <tahaburrent@gmail.com>
2022-11-30 17:43:23 +0200

    ~

---
+++ ticket.js
@@ -0,0 +1,11 @@
+const ticketMaker = (amount) => {
+  const mainList = document.querySelector(".main-list");
+  for (let i = 1; i <= amount; i += 1) {
+    const htmlString = `<a target="_blank"
+        rel="noreferrer noopener nofollow"
+        href="./Questions-for-the-exam/№${i}.html"
+        >№${i}.</a>`;
+    mainList.innerHTML += htmlString;
+  }
+};
+ticketMaker(115);
