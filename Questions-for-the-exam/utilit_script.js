676056977c571abd7ee08e7a0217936590d01362
Valera Kamelkov <tahaburrent@gmail.com>
2022-12-06 22:58:53 +0200

    ~

--- Questions-for-the-exam/utilit_script.js
+++
@@ -1,9 +0,0 @@
-nBody = (amount) => {
-  for (let i = 1; i <= amount; i += 1) {
-    console.log(i);
-    let bodyId = document.querySelector(`body#№${i}`);
-    console.log(bodyId);
-  }
-};
-
-nBody(2);
